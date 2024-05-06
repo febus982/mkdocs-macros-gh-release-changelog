5feec2af14ce45b8075a4486cca88c1633311727
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-05-06 15:14:10 +0100

    Repository configuration (#2)
    * Rename bootstrap references
    
    * Cleanup README.md
    
    * Backport Copyright config
    
    * Rename workflow
    
    * Update pyproject.toml
    
    * Update docs index

--- mkdocs_macros_gh_release_changelog/__init__.py
+++ mkdocs_macros_gh_release_changelog/__init__.py
@@ -21,3 +21,15 @@
 #  DEALINGS IN THE SOFTWARE.                                                   =
 # ==============================================================================
 
+__version__ = "0.0.0"
+__version_tuple__ = (0, 0, 0)
+
+
+def some_function() -> str:
+    """
+    Some function docstring
+
+    :return: Some string
+    :rtype: str
+    """
+    return "some_variable_to_test"
